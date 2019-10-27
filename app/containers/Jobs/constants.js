1d0f6696ac06af382f5d4945c63b5d049e1bbca7
Alireza <alirezark@gmail.com>
2019-10-27 16:03:01 +0330

    feat: Removes redux extra files of jobs container

--- app/containers/Jobs/constants.js
+++
@@ -1,9 +0,0 @@
-/*
- *
- * Jobs constants
- *
- */
-
-export const DEFAULT_ACTION = 'app/Jobs/DEFAULT_ACTION';
-export const REQUEST_JOBS = 'app/jobs.REQUEST_JOBS';
-export const RESPONSE_JOBS = 'app/jobs.RESPONSE_JOBS';
